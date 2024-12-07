ad36b2e4afb5aad5930d5ba59880337dc74aebf0
Garance Buricatu <garance.mary@gmail.com>
2024-12-06 19:52:59 -0500

    add route

--- app.py
+++ app.py
@@ -1,12 +1,11 @@
 import tweepy
 import os
-from urllib.parse import urlparse
 
 from flask import Flask, redirect, request, jsonify
 
 app = Flask(__name__)
 
-REDIRECT_URI = "https://83cf-2607-fa49-ad02-3000-484e-23ca-d67b-339e.ngrok-free.app/callback"
+REDIRECT_URI = "https://c599-2607-fa49-ad02-3000-4719-3fbf-dbe3-c303.ngrok-free.app/callback"
 
 oauth2_user_handler = tweepy.OAuth2UserHandler(
     client_id=os.getenv("TWITTER_CLIENT_ID"),
@@ -15,8 +14,23 @@ oauth2_user_handler = tweepy.OAuth2UserHandler(
     client_secret=os.getenv("TWITTER_CLIENT_SECRET"),
 )
 
-@app.route('/login')
-def login():
+@app.route('/app/login')
+def login_app():
+    """
+    Redirect the user to Twitter's authorization page.
+    """
+
+    oauth2_app_handler = tweepy.OAuth2AppHandler(
+        consumer_key=os.getenv("TWITTER_CONSUMER_KEY"),
+        consumer_secret=os.getenv("TWITTER_CONSUMER_SECRET"),
+    )
+
+    handler = oauth2_app_handler.apply_auth()
+
+    return jsonify({"bearer_token": handler.bearer_token})
+
+@app.route('/user/login')
+def login_user():
     """
     Redirect the user to Twitter's authorization page.
     """
@@ -61,4 +75,6 @@ def tweet():
     return jsonify({"tweet": tweet.data.__str__()}), 200
 
 if __name__ == '__main__':
-    app.run(port=8000, debug=True)
+    app.run(port=8080, debug=True)
+
+# ngrok http http://localhost:8080
